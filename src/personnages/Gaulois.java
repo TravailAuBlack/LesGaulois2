2fa71fec227ef7f16f9e56b42a9e3fb3023f9c2d
TravailAuBlack <yassine82600@gmail.com>
2022-11-14 23:30:08 +0100

    TP2/3 PB sur enum equipement

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -5,8 +5,9 @@ import personnages.Romain;
 
 public class Gaulois {
 	private String nom;
-	private int force;
 	private int effetPotion = 1;
+	private int force, nb_trophees;
+	private Equipement trophees[] = new Equipement[100];
 
 	public Gaulois(String nom, int force) {
 		super();
@@ -22,28 +23,38 @@ public class Gaulois {
 		System.out.println(prendreParole() + "� " + texte + "�");
 	}
 
-	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
-	}
-	
-	
-	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup((force / 3) * effetPotion);
-	}
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
+
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+//		romain.recevoirCoup((force / 3) * effetPotion);
+//	}
 
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
-	
-	
-	public void boirePotion(int forcePotion) 
-	{
-		effetPotion = effetPotion + forcePotion ;
+
+	public void boirePotion(int forcePotion) {
+		effetPotion = effetPotion + forcePotion;
 		parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée.");
 	}
-	
+
+	private String prendreParole() {
+		String texte = "Le gaulois " + nom + " : ";
+		return texte;
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) * effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++, nb_trophees++) {
+			this.trophees[nb_trophees] = trophees[i];
+		}
+		return;
+	}
 
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
